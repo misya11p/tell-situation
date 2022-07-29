d8a24280c4169d60d56d43478b81d0e63b2b0727
misya11p <miya33ps@gmail.com>
2022-07-30 01:27:52 +0900

    add argparse

--- app.py
+++ app.py
@@ -3,20 +3,17 @@ from object_detection import object_detection
 from speech import voice_write
 from playsound import playsound
 import time
+import argparse
 
-INTERVAL = 5
 
-def main():
-    cap = cv2.VideoCapture(0)
+def main(device_id=0, interval=5.):
+    cap = cv2.VideoCapture(device_id)
     start = time.time()
     while True:
         _, img = cap.read()
-        if time.time() - start > INTERVAL:
-            objects = object_detection(img)
-            names = list(set(objects['name'])) # 名前だけ抽出
-            sentence = make_sentence(names)
-            wav_path = voice_write(sentence)
-            playsound(wav_path)
+
+        if time.time() - start > interval:
+            speech_situation(img)
             start = time.time()
 
         cv2.imshow('frame', img)
@@ -25,6 +22,15 @@ def main():
 
     cap.release()
     cv2.destroyAllWindows()
+
+
+def speech_situation(img):
+    objects = object_detection(img)
+    names = list(set(objects['name']))
+    sentence = make_sentence(names)
+    wav_path = voice_write(sentence)
+    playsound(wav_path)
+
 
 def make_sentence(objects):
     n_obj = len(objects)
@@ -38,4 +44,8 @@ def make_sentence(objects):
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='カメラから得た状況を音声で伝える')
+    parser.add_argument('--device_id', type=int, default=0, help='デバイスのid')
+    parser.add_argument('--interval', type=float, default=5., help='次に音声を読み上げるまでの時間(s)')
+    args = parser.parse_args()
+    main(**vars(args))
